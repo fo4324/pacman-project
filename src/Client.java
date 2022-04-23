b09d279705d0ac4d608cb25c75c0f51d7355c76b
fo4324 <fo4324@g.rit.edu>
2022-04-23 16:09:19 +0200

    initial commit

---
+++ src/Client.java
@@ -0,0 +1,182 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ * A client for the PACMAN final project for Computational Problem Solving in the information domain II
+ * @author F. Orlandini & Zaid Alkabani
+ * @version 20042022
+ * @ASSESSME.INTENSITY:LOW
+ */
+
+public class Client extends Application {
+
+    // Client Attributes
+    private final int SERVER_PORT = 36912;
+    private Scanner in = null;
+    private PrintWriter out = null;
+    private String name = "";
+    private Socket socket = null;
+
+    // GUI Attributes
+    private VBox root = new VBox(10);
+    private TextField tfInput = new TextField();
+    private TextField tfConnect = new TextField();
+    private Button btSend = new Button("Send");
+    private Button btConnect = new Button("Connect");
+    private TextArea taLog = new TextArea();
+    private TextArea taClients = new TextArea();
+    private HBox hbConnect = new HBox(10);
+    private HBox hbInput = new HBox(10);
+
+    // ** MAIN **
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    // Launching the Application
+    public void start(Stage stage) {
+        // Application title
+        stage.setTitle("PACMAN Client");
+
+        // Handling the window close
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                if(socket != null) {
+                    out.println("LOGOUT@");
+                    out.flush();
+                }
+                System.exit(0);
+            }
+        });
+
+        // GUI Layout
+        hbInput.getChildren().addAll(tfInput, btSend);
+        hbInput.setAlignment(Pos.CENTER);
+
+        hbConnect.getChildren().addAll(tfConnect, btConnect);
+        hbConnect.setAlignment(Pos.CENTER);
+        
+        root.getChildren().addAll(hbConnect, taClients, taLog, hbInput);
+
+        taClients.setEditable(false);
+        taClients.setPrefHeight(150);
+
+        taLog.setEditable(false);
+        taLog.setPrefHeight(250);
+
+        stage.setX(500);
+        stage.setY(0);
+        stage.setResizable(false);
+
+        // Window Icon
+        stage.getIcons().add(new Image("logo.png"));
+
+        // Showing the Application
+        stage.setScene(new Scene(root, 500, 500));
+        stage.show();
+
+        // Button Actions
+        btConnect.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent ae) {
+                doConnect();
+            }
+        });
+
+        btSend.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent ae) {
+                doSend();
+            }
+        });
+    }
+
+    // Method for connecting to the server
+    public void doConnect() {
+        if(!tfConnect.getText().equals("")) {
+            try {
+                socket = new Socket(tfConnect.getText(), SERVER_PORT);
+                in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream());
+
+                // A Text Input Dialog for the username
+                TextInputDialog loginDialog = new TextInputDialog();
+                loginDialog.setHeaderText("Enter your username: ");
+                loginDialog.showAndWait();
+
+                name = loginDialog.getEditor().getText();
+                out.println("LOGIN@" + name);
+                out.flush();
+                taLog.appendText("Connected to the server as " + name + "\n");
+                btConnect.setDisable(true);
+                tfConnect.setDisable(true);
+                receiveMessage();
+            } catch(IOException ioe) {
+                taLog.appendText("ERROR - Cannot connect to the server.\n");
+            }
+        } else {
+            taLog.appendText("Please enter a valid server address.\n");
+        }
+    }
+
+    // Method for receiving commands and messages from the server
+    public void receiveMessage() {
+        new Thread(new Runnable() {
+            public void run() {
+                while(in.hasNextLine()) {
+                    String line = in.nextLine();
+                    String[] cmd = line.split("@");
+                    switch(cmd[0]) {
+                        case "ADD":
+                            taClients.clear();
+                            doAdd(cmd[1]);
+                            break;
+                        case "MESSAGE":
+                            doReceive(cmd[1]);
+                            break;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // Method for sending messages to the server
+    public void doSend() {
+        String text = tfInput.getText();
+        if(!text.equals("")) {
+            out.println("MESSAGE@" + text);
+            out.flush();
+        }
+        tfInput.clear();
+    }
+
+    // Methods for listing all the connected clients
+    public void doAdd(String name) {
+        new Thread(new Runnable() {
+            public void run() {
+                taClients.appendText(name + "\n");
+            }
+        }).start();
+    }
+
+    // Method for receiving a message
+    public void doReceive(String message) {
+        taLog.appendText(message + "\n");
+    }
+
+}
---
+++ src/Server.java
@@ -0,0 +1,215 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ * A server for the PACMAN final Project for Computational Problem Solving in the information domain II
+ * @author F. Orlandini & Zaid Alkabani
+ * @version 14042022
+ * @ASSESSME.INTENSITY:LOW
+ */
+
+public class Server extends Application {
+    
+    // Server Attributes
+    private final int SERVER_PORT = 36912;
+    private Thread server;
+    private ServerSocket serverSocket = null;
+    private ArrayList<ClientHandler> clients = new ArrayList<>();
+
+    // GUI Attributes
+    private VBox root = new VBox(10);
+    private Button btStart = new Button("Start");
+    private TextArea taLog = new TextArea();
+    private TextArea taClients = new TextArea();
+
+    // ** MAIN **
+    public static final void main(String[] args) {
+        launch(args);
+    }
+
+    // Launching the Application
+    public void start(Stage stage) {
+        // Application Title
+        stage.setTitle("PACMAN Server");
+
+        // Handling the window close
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+
+        // GUI Layout
+        root.getChildren().addAll(btStart, taClients, taLog);
+        root.setAlignment(Pos.CENTER);
+
+        taClients.setPrefHeight(150);
+        taClients.setEditable(false);
+
+        taLog.setPrefHeight(350);
+        taLog.setEditable(false);
+
+        stage.setResizable(false);
+        stage.setX(0);
+        stage.setY(0);
+
+        // Window Icon
+        stage.getIcons().add(new Image("logo.png"));
+
+        // Showing the Application
+        stage.setScene(new Scene(root, 500, 500));
+        stage.show();
+
+        // Button Actions
+        btStart.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent ae) {
+                server = new Thread() {
+                    public void run() {
+                        startServer();
+                    }
+                };
+                server.start();
+            }
+        });
+
+    }
+
+    // Method for starting the server
+    public void startServer() {
+        btStart.setDisable(true);
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+            taLog.appendText("Server started.\n");
+    
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread client = new ClientHandler(clientSocket);
+                taLog.appendText(clientSocket.getInetAddress() + " connected.\n");
+                client.start();
+            }
+        } catch(IOException ioe) {
+            taLog.appendText("ERROR - Cannot start server!\n");
+            ioe.printStackTrace();
+        }
+    }
+
+    // Client Thread class
+    class ClientHandler extends Thread {
+
+        // Client Thread Attributes
+        private Socket clientSocket;
+        private Scanner in;
+        private PrintWriter out;
+        private String name;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            clients.add(this);
+        }
+
+        public void run() {
+            try {
+                this.in = new Scanner(this.clientSocket.getInputStream());
+                this.out = new PrintWriter(this.clientSocket.getOutputStream());
+                while(this.clientSocket.isConnected()) {
+                    if(this.clientSocket.isClosed()) {
+                        break;
+                    } else {
+                        String[] cmd = in.nextLine().split("@");
+                        switch(cmd[0]) {
+                            case "LOGIN":
+                                doLogin(cmd[1]);
+                                break;
+                            case "LOGOUT":
+                                doLogout();
+                                break;
+                            case "MESSAGE":
+                                doMessage(cmd[1]);
+                                break;
+                        }
+                    }
+                }
+            } catch(IOException ioe) {
+                taLog.appendText("ERROR - Cannot open input and output streams for " + this.clientSocket.getInetAddress() + "\n");
+                ioe.printStackTrace();
+            }
+        }
+
+        // Method for client login
+        public void doLogin(String text) {
+            this.name = text;
+            taLog.appendText(this.clientSocket.getInetAddress() + " logged in as: " + this.name + "\n");
+            flushToAll("MESSAGE@" + this.name + " logged in");
+            listClients();
+        }
+
+        // Method for client logout
+        public void doLogout() {
+            clients.remove(this);
+            String line = this.name + " logged out";
+            flushToAll("MESSAGE@" + line);
+            taLog.appendText(line + "\n");
+            listClients();
+            this.in.close();
+            this.out.close();
+            try {
+                this.clientSocket.close();
+            } catch (IOException ioe) {
+                taLog.appendText("ERROR - Cannot close " + this.clientSocket.getInetAddress() + "\n");
+                ioe.printStackTrace();
+            }
+        }
+
+        // Method for chatting between clients and the server
+        public void doMessage(String message) {
+            taLog.appendText(this.name + ": " + message + "\n");
+            flushToAll("MESSAGE@" + this.name + ": " + message);
+        }
+
+        // Method for flushing to all connected clients
+        public void flushToAll(String line) {
+            new Thread(new Runnable() {
+                public void run() {
+                    for(ClientHandler client : clients) {
+                        client.out.println(line);
+                        client.out.flush();
+                    }
+                }
+            }).start();
+        }
+
+        // Method for maintaining the connected client list
+        public void listClients() {
+            new Thread(new Runnable() {
+                public void run() {
+                    taClients.clear();
+                    for(ClientHandler client : clients) {
+                        for(int i = 0; i < clients.size(); i++) {
+                            client.out.println("ADD@" + clients.get(i).name);
+                            client.out.flush();
+                        }
+                        taClients.appendText(client.name + "\n");
+                    }
+                }
+            }).start();
+        }
+
+    }
+
+}
